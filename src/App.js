5df80743f3417f3da6720f81d85e02547788576d
Oli Bullen <d33con@gmail.com>
2017-02-24 13:40:04 +0000

    remove end session

--- src/App.js
+++ src/App.js
@@ -1,55 +1,93 @@
 import React, { Component } from 'react';
 import './App.css';
 import CountdownDisplay from './CountdownDisplay';
-import EndTimeDisplay from './EndTimeDisplay';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      endTime: 0,
-      seconds: 0
+      secondsRemaining: 0,
+      breakTime: 0,
+      workTime: 0,
+      isWorkTime: true
     };
     this.incrementWorkTime = this.incrementWorkTime.bind(this);
     this.decrementWorkTime = this.decrementWorkTime.bind(this);
+    this.incrementBreakTime = this.incrementBreakTime.bind(this);
+    this.decrementBreakTime = this.decrementBreakTime.bind(this);
     this.startCountdown = this.startCountdown.bind(this);
     this.pauseCountdown = this.pauseCountdown.bind(this);
     this.countdown = this.countdown.bind(this);
   }
 
+  // mount the component with default values and set counter to work time
   componentDidMount() {
-    this.setState({seconds: 900});
+    this.setState({
+      secondsRemaining: 120,
+      workTime: 120,
+      breakTime: 60
+    });
   }
 
   componentWillUnmount() {
     clearInterval(this.interval);
   }
 
+  // functions to control setting of workTime
   incrementWorkTime() {
-    this.setState({seconds: this.state.seconds + 60});
+    this.setState({
+      workTime: this.state.workTime + 60,
+      secondsRemaining: this.state.secondsRemaining + 60
+    });
   }
   decrementWorkTime() {
-    this.setState({seconds: this.state.seconds - 60});
+    this.setState({
+      workTime: this.state.workTime - 60,
+      secondsRemaining: this.state.secondsRemaining - 60
+    });
   }
 
+  // functions to control setting of breakTime
+  incrementBreakTime() {
+    this.setState({breakTime: this.state.breakTime + 60});
+  }
+  decrementBreakTime() {
+    this.setState({breakTime: this.state.breakTime - 60});
+  }
+
+  // countdown function
   countdown() {
-    this.setState({seconds: this.state.seconds - 1});
+    this.setState({secondsRemaining: this.state.secondsRemaining - 1});
   }
 
+  // function to handle switching between work & break time and run the countdown
   startCountdown() {
+    // clear any running timers
     clearInterval(this.interval);
 
-    const now = Date.now();
-    let endTime = now + (this.state.seconds * 1000);
-    this.setState({endTime: endTime});
+    // run this function every second
     this.interval = setInterval(() => {
-      if (this.state.seconds < 0) {
-        clearInterval(this.interval);
+      if (this.state.secondsRemaining <= 0 && this.state.isWorkTime === true) {
+        // if it's the end of work time set timer to breakTime and start the countdown
+        this.setState({
+          isWorkTime: false,
+          secondsRemaining: this.state.breakTime
+        });
+        this.startCountdown();
+      } else if (this.state.secondsRemaining <= 0 && this.state.isWorkTime === false) {
+        // if it's the end of break time set timer to workTime and start the countdown
+        this.setState({
+          isWorkTime: true,
+          secondsRemaining: this.state.workTime
+        });
+        this.startCountdown();
       }
+      // otherwise continue counting down
       this.countdown()
     }, 1000);
   }
 
+  // pause function
   pauseCountdown() {
     clearInterval(this.interval);
   }
@@ -58,19 +96,18 @@ class App extends Component {
     return (
       <div className="App">
         <div>
-          <button onClick={this.incrementWorkTime}>+</button>
-          <span>{`Work Time - ${this.state.seconds / 60} minutes`}</span>
           <button onClick={this.decrementWorkTime}>-</button>
+          <span>{`Work Time - ${this.state.workTime / 60} minutes`}</span>
+          <button onClick={this.incrementWorkTime}>+</button>
         </div>
         <div>
-          <button onClick={this.incrementWorkTime}>+</button>
-          <span>{`Break Time - ${this.state.seconds / 60} minutes`}</span>
-          <button onClick={this.decrementWorkTime}>-</button>
+          <button onClick={this.decrementBreakTime}>-</button>
+          <span>{`Break Time - ${this.state.breakTime / 60} minutes`}</span>
+          <button onClick={this.incrementBreakTime}>+</button>
         </div>
-        <div><CountdownDisplay seconds={this.state.seconds} /></div>
+        <div><CountdownDisplay seconds={this.state.secondsRemaining} /></div>
         <div><button onClick={this.startCountdown}>Start</button></div>
         <div><button onClick={this.pauseCountdown}>Pause</button></div>
-        <div><EndTimeDisplay time={this.state.endTime}/></div>
       </div>
     );
   }
