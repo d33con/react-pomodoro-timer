f13bc2138d45da82ca2487982d606ec66b8a6bba
Oli Bullen <d33con@gmail.com>
2017-03-01 16:59:55 +0000

    fill timer bg according to countdown

--- src/CountdownDisplay.js
+++ src/CountdownDisplay.js
@@ -1,28 +1,57 @@
 import React from 'react';
+
+import './css/CountdownDisplay.css';
 import MinuteDisplay from './MinuteDisplay';
 import SecondDisplay from './SecondDisplay';
 
-import './css/CountdownDisplay.css';
-
-const CountdownDisplayProps = {
+CountdownDisplay.propTypes = {
   seconds: React.PropTypes.number.isRequired,
-  session: React.PropTypes.bool.isRequired
+  isWorkTime: React.PropTypes.bool.isRequired,
+  workTime: React.PropTypes.number.isRequired,
+  breakTime: React.PropTypes.number.isRequired
 }
 
-function CountdownDisplay ({seconds, session}) {
+function CountdownDisplay ({seconds, isWorkTime, workTime, breakTime}) {
   const minutesRemaining = Math.floor(seconds / 60);
   const secondsRemaining = seconds % 60;
+  let bgFillAmount;
+  let bgFillColor;
+  // calculate the amount of the background to fill according to the amount of 
+  // time passed in the current session. 
+  //Set the background color according to the session type
+  if (isWorkTime) {
+    bgFillAmount = ((20 / workTime) * (workTime - seconds) / 2) + 'rem';
+    bgFillColor = 'rgba(237, 36, 94, 0.8)'; // red
+  } else {
+    bgFillAmount = ((20 / breakTime) * (breakTime - seconds) / 2) + 'rem';
+    bgFillColor = 'rgba(244, 208, 63, 0.8)'; // yellow
+  }
+  
+  // set the styles for the timer background
+  const backgroundStyle = {
+    countingDown: {
+      fontSize: '5rem',
+      border: '5px solid #222829',
+      borderRadius: '50%',
+      width: '20rem',
+      height: '20rem',
+      margin: '40px auto',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      boxShadow: `inset 0 0 0.5rem ${bgFillAmount} ${bgFillColor}`,
+      transition: 'box-shadow ease 0.5s'
+    }
+  };
 
   return (
-    <div className="timer-container">
-      <div className="timer-display">
-        <div className="session-type">{session ? `Work` : `Break`}</div>
+    <div style={backgroundStyle.countingDown}>
+      <div>
+        <div className="session-type">{isWorkTime ? `Work` : `Break`}</div>
         <MinuteDisplay minutes={minutesRemaining} />:<SecondDisplay seconds={secondsRemaining} />
       </div>
     </div>
   );
 }
-
-CountdownDisplayProps.propTypes = CountdownDisplayProps.time;
 
 export default CountdownDisplay;
